d5b77102147148a649cca255e27080729e7fb5bd
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-07-03 12:23:39 +0000

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -10,21 +10,27 @@ def solve_nqueens(n):
 
     try:
         N = int(sys.argv[1])
-        if N < 4:
-            raise ValueError
     except ValueError:
-        print("N must be an integer greater or equal to 4")
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
         sys.exit(1)
 
     def is_safe(board, row, col):
         for i in range(row):
-            if board[i] == col or board[i] - col == i - row or col - board[i] == i - row:
+            if (
+                board[i] == col
+                or board[i] - col == row - i
+                or col - board[i] == row - i
+            ):
                 return False
         return True
 
     def solve(board, row):
         if row == N:
-            solutions.append(board[:])
+            solutions.append(board.copy())
         else:
             for col in range(N):
                 if is_safe(board, row, col):
@@ -32,14 +38,11 @@ def solve_nqueens(n):
                     solve(board, row + 1)
 
     solutions = []
-    board = [-1] * N
-    solve(board, 0)
+    solve([-1] * N, 0)
 
-    return solutions
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(N)])
 
 
 if __name__ == "__main__":
-    solutions = solve_nqueens(sys.argv[1])
-
-    for sol in solutions:
-        print([[i, sol[i]] for i in range(len(sol))])
+    solve_nqueens(sys.argv[1])
